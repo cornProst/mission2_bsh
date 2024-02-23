4c3a7db1470566b887c2ed3f830dcdf3d1d5fc3a
qotjdgns <baesunghoon81@gmail.com>
2024-02-23 13:36:23 +0900

    commit test

---
+++ src/main/java/com/example/mission2_bsh/Mission2BshApplication.java
@@ -0,0 +1,13 @@
+package com.example.mission2_bsh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mission2BshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mission2BshApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mission2_bsh/Mission2BshApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mission2_bsh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mission2BshApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
